27ad4e2157cd57ea1db9db37da0a763939643d33
oarino <oscar.arino@gmail.com>
2022-03-29 18:56:51 +0000

    modificación

--- wp-config.php
+++ wp-config.php
@@ -94,3 +94,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+/**Intentar que acceda a los plugins */
+define('FS_METHOD', 'direct');
